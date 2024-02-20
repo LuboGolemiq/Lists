51715a861b9f88718b12cdc5af89e55e497cfe34
LuboGolemiq <lubomirpalazov19@gmail.com>
2024-02-20 23:10:18 +0200

    or

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ListTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int index = 0; index < list.Count; index++)
+            {
+                Console.WriteLine($"list [] = {index}");
+            }
+        }
+    }
+}
